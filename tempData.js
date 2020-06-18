41e0027271b6f71e5bf94b0f87db0aee678031ec
Koua Thao <kouathao09@gmail.com>
2020-06-17 21:25:54 -0500

    Completed video 1

---
+++ tempData.js
@@ -0,0 +1,70 @@
+// Temporary Data
+
+export default tempData = [
+  {
+    name: "Plan a trip",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Book Flight",
+        completed: true,
+      },
+      {
+        title: "Passport Check",
+        completed: true,
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: false,
+      },
+      {
+        title: "Pack luggage",
+        completed: true,
+      },
+    ],
+  },
+  {
+    name: "Errand",
+    color: "#34abeb",
+    todos: [
+      {
+        title: "Book Flight",
+        completed: true,
+      },
+      {
+        title: "Passport Check",
+        completed: true,
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: true,
+      },
+      {
+        title: "Pack luggage",
+        completed: true,
+      },
+    ],
+  },
+  {
+    name: "Party",
+    color: "#b4eb34",
+    todos: [
+      {
+        title: "Book Flight",
+        completed: true,
+      },
+      {
+        title: "Passport Check",
+        completed: true,
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: true,
+      },
+      {
+        title: "Pack luggage",
+        completed: true,
+      },
+    ],
+  },
+];
